e07c5ae2cda8914b2f8ae0f18bb9d50a05a4ed9d
shaikswabirachand <skswabirachand@yahoo.com>
2025-03-03 06:17:22 +0530

    new problem added

---
+++ NFS_KMIT/TwoPointer/SortNumbersAndIgnoreZeros.java
@@ -0,0 +1,21 @@
+/*
+ Given an integer array nums, move all 0's to the end of it while maintaining the 
+relative order of the non-zero elements. 
+Note that you must do this in-place without making a copy of the array.
+
+Sample test case
+case=1
+input=5
+0 1 0 3 12
+output=[1, 3, 12, 0, 0]
+
+case=2
+input=1
+0
+output=[0]
+
+ */
+
+public class SortNumbersAndIgnoreZeros {
+    
+}
---
+++ NFS_KMIT/TwoPointer/returnSortedSquares.java
@@ -0,0 +1,15 @@
+/*
+Given an integer array nums, sorted in non-decreasing order, return an array 
+of the squares of each number sorted in non-decreasing order.
+
+Sample test case
+case=1
+input=6
+-3 0 1 4 5 7
+output=[0, 1, 9, 16, 25, 49]
+
+case=2
+input=4
+-5 -2 3 6
+output=[4, 9, 25, 36]
+*/
